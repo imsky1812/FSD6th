5c5bb3336e31be25a7f5cc366fe6d4ee8d9a2798
imsky1812 <sarveshkyadav18@gmail.com>
2026-02-03 09:39:25 +0530

    updatation in day01

--- Day01/promises.js
+++ Day01/promises.js
@@ -1,20 +1,21 @@
 const myPromise = new Promise((resolve, reject) => {
-    let age = 5;
-    try {
-        if (age > 18) {
-            resolve("eligible for vote")
-        } else {
-            reject("Not eligible")
-        }
-    } catch (error) {
-        console.log("not running");
-
+    let age = 18;
+    if (age > 18) {
+        resolve("eligible for vote")
+    } else {
+        reject("Not eligible")
     }
 })
+const f1 = async () => {
+    try {
+        const msg = await myPromise;
+        console.log(msg);
+    } catch (error) {
+        console.log("some error occured.");
+    }
+}
+f1();
+// myPromise.then((msg) => console.log(msg))
+//     .catch((error) => console.log(error))
 // console.log(myPromise);
 
-myPromise.then((msg) => console.log(msg))
-    .then(() => console.log("yes successfully handled"))
-    .catch((error) => console.log(error))
-    .finally(()=>console.log("final")
-    )
